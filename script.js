136a452ccec377319369cce1bb5c63b197bc93c6
skrralex <148615303+skrralex@users.noreply.github.com>
2023-10-21 17:28:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,13 @@
  */
 
 let state = Object.freeze({
-    pointerEvent: { x: 0, y: 0 },
+    pointerEvent: { y: 0 },
+    clickEvent: { x: 0 },
 });
 
 
 const settings = Object.freeze({
-    textElement: document.querySelector(".text"),
+    textElement: document.querySelector("#text"),
 });
 
 
@@ -60,17 +61,24 @@ function toAbsolute(num, min, max) {
  */
 function loop() {
     const { pointerEvent } = state;
+    const { clickEvent } = state;
     const { textElement } = settings;
 
+    settings.textElement.setAttribute('contenteditable', 'true');
+
+    settings.textElement.setAttribute('spellcheck', 'false');
+
+
     // Map the font weight to the width
-    const x = scale(pointerEvent.x, 0, window.innerWidth);
-    const fontWeight = toAbsolute(x, 100, 1000); // the `fontWeight` property expects a number between 100 and 1000
-    textElement.style.fontWeight = `${fontWeight}`;
+    const x = scale(clickEvent.x, 0, window.innerWidth);
+    const fontSize = toAbsolute(x, 40, 160); // the `fontSize` property expects a number between 100 and 1000
+    textElement.style.fontSize = `${fontSize}px`;
 
     // And the font slant to the height
     const y = scale(pointerEvent.y, 0, window.innerHeight);
-    const fontSlant = toAbsolute(y, 0, 10); // the `sontStyle` property expects the string "oblique Xdeg" where X is a number betwen 0 and 40
-    textElement.style.fontStyle = `oblique ${fontSlant}deg`;
+    const fontMorph = toAbsolute(y, 0, 60); // the `sontStyle` property expects the string "oblique Xdeg" where X is a number betwen 0 and 40
+    textElement.style.fontVariationSettings = `"MORF" ${fontMorph}`;
+
 
     window.requestAnimationFrame(loop);
 }
@@ -81,9 +89,23 @@ function loop() {
  */
 function setup() {
 
+    // settings.textElement.style.display = 'flex';          //Turn textElement into a flex container
+    // settings.textElement.style.justifyContent = 'center'; //Center the text horizontally
+    // settings.textElement.style.alignItems = 'center';     //Center the text vertically
+    // settings.textElement.style.height = '65vh';          //Set the height of the container to 100% of the viewport height
+
     document.addEventListener("pointermove", function (event) {
         updateState({ pointerEvent: event });
     });
+
+     document.addEventListener("pointerdown", function (event) {
+         updateState({ clickEvent: event });
+     });
+
+    
+    
+    
+
 
     loop();
 }
